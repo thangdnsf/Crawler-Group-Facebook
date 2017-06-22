fc6a3460c85f634d59de11d7bf0f6a08e2e46d94
Thang Nguyen <thangdn.tlu@outlook.com>
2017-06-22 23:45:58 +0700

    update v2

--- crawlgroupfacebook.py
+++ crawlgroupfacebook.py
@@ -36,20 +36,19 @@
 #**********************************************************************/
 import time, os, sys, datetime
 import requests
-import json
 import facebook
 import unicodecsv as csv
 from multiprocessing import Pool
 
 group_id = '1098837086877710'
-access_token = ''
+access_token = 'YOUR TOKEN'
 
 
-graph = facebook.GraphAPI(access_token, version='2.7')
+graph = facebook.GraphAPI(access_token, version='2.3')
 
 #create post csv
 writerpost = csv.writer(open('posts.csv', 'ab+'), encoding='utf-8')
-writerpost.writerow(['p_id','p_message','p_created_time'])
+writerpost.writerow(['p_id','type','p_message','p_created_time','p_updated_time','u_id','u_name','no_share'])
 
 #create comment_post csv
 writercomment = csv.writer(open('comments.csv', 'ab+'), encoding='utf-8')
@@ -59,121 +58,123 @@ writercomment.writerow(['p_id','parent_cid','cid','c_message','c_created_time','
 writerreaction = csv.writer(open('reactions.csv', 'ab+'), encoding='utf-8')
 writerreaction.writerow(['p_id','cid','type','uid','uname'])
 
-#create share.csv csv
-writershare = csv.writer(open('shares.csv', 'ab+'), encoding='utf-8')
-writershare.writerow(['p_id','mgs','uid','uname'])
-
+#set limit:
+plimit=50
 #get posts
+#posts = graph.get_connections(group_id, "feed",limit=plimit)
+#url = '/feed?fields=id,from,message,type,created_time,updated_time,story,shares,comments.limit('+str(plimit)+'){id,message,from,created_time}&limit='+str(plimit)
 
-posts = graph.get_connections(group_id, "feed")
+url = 'https://graph.facebook.com/v2.7/'+group_id+'?fields=feed.limit('+str(plimit)+')%7Bid%2Cfrom%2Cmessage%2Ctype%2Ccreated_time%2Cupdated_time%2Cstory%2Cshares%2Creactions%2Ccomments%7Bid%2Cfrom%2Cmessage%2Creactions%2Ccomments%7Bid%2Cfrom%2Cmessage%2Creactions%2Ccreated_time%7D%2Ccreated_time%7D%7D&access_token='+access_token
+print(url)
 
-#get post's reactions
-def getreactionsPost(p):
-    reacpost = graph.get_connections(p['id'], "reactions")
-    results = []
-    while (True):
-        if (len(reacpost['data']) == 0):
-            break
-        for re in reacpost['data']:
-            results.append(
-                [p['id'], 'x', re['type'], re['id'], re['name']])
-        try:
-            reacpost = requests.get(reacpost['paging']['next']).json()
-        except:
-            break
-    return results
-# get post's share
-def getsharePost(p):
-    sharepost = graph.get_connections(p['id'], "share")
-    while (True):
-        if (len(sharepost['data']) == 0):
-            break
-        for re in sharepost['data']:
-            writerreaction.writerow(
-                [p['id'], 'x', re['type'], re['id'], re['name']])
-        try:
-            sharepost = requests.get(sharepost['paging']['next']).json()
-        except:
-            break
-def getreactionReplyCommnet(p,c,r):
-    resultreaction=[]
-    reaccomment = graph.get_connections(r['id'], "reactions")
-    while (True):
-        if (len(reaccomment['data']) == 0):
-            break
-        for rre in reaccomment['data']:
-            resultreaction.append(
-                [p['id'], c['id'], rre['type'], rre['id'], rre['name']])
-        try:
-            reaccomment = requests.get(reaccomment['paging']['next']).json()
-        except:
-            break
-    return resultreaction
+posts = requests.get(url).json()['feed']
 
-def getreplyComment(p,c):
+def getreaction(p,a):
+    tempreac = []
+    try:
+        reaction = a['reactions']
+        while (True):
+            if (len(reaction['data']) == 0):
+                break
+            for r in reaction['data']:
+                tempreac.append(
+                    [p['id'], a['id'], r['type'], r['id'], r['name']])
+            try:
+                reaction = requests.get(reaction['paging']['next']).json()
+            except:
+                break
+    except:
+        tempreac = []
+    return tempreac
+def getreplyComment(p,pc):
     resultcomment=[]
     resultreaction=[]
-    rcomment = graph.get_connections(c['id'], "comments")
-    while (True):
-        if (len(rcomment['data']) == 0):
-            break
-        for r in rcomment['data']:
-            resultcomment.append(
-                [p['id'], c['id'], r['id'], r['message'], r['created_time'], r['from']['id'],
-                 r['from']['name']])
-            # get reply comment's reactions
-            resultreaction.extend(getreactionReplyCommnet(p,c, r))
-        try:
-            rcomment = requests.get(rcomment['paging']['next']).json()
-        except:
-            break
-    return (resultcomment,resultreaction)
+    try:
+        com = pc['comments']
+        while (True):
+            if (len(com['data']) == 0):
+                break
+            for c in com['data']:
+                resultcomment.append(
+                    [p['id'], pc['id'], c['id'], c['message'], c['created_time'], c['from']['id'], c['from']['name']])
+                resultreaction.extend(getreaction(p,c))
 
-def getreactionComment(p,c):
-    # get comment's reactions
-    resultreaction = []
-    reaccomment = graph.get_connections(c['id'], "reactions")
-    while (True):
-        if (len(reaccomment['data']) == 0):
-            break
-        for cre in reaccomment['data']:
-            resultreaction.append(
-                [p['id'], c['id'], cre['type'], cre['id'], cre['name']])
-        try:
-            reaccomment = requests.get(reaccomment['paging']['next']).json()
-        except:
-            break
-    return resultreaction
+            try:
+                com = requests.get(com['paging']['next']).json()
+            except:
+                break
+    except:
+        resultcomment = []
+    return (resultcomment,resultreaction)
 
 def getcommentPost(p):
     resultcomment=[]
-    resultreaction=[]
-    com = graph.get_connections(p['id'], "comments")
-    while (True):
-        if (len(com['data']) == 0):
-            break
-        for c in com['data']:
-            resultcomment.append(
-                [p['id'], 'x', c['id'], c['message'], c['created_time'], c['from']['id'], c['from']['name']])
-            # get comment's reactions
-            resultreaction.extend(getreactionComment(p,c))
-            # get reply comment
-            kq = getreplyComment(p, c)
-            resultcomment.extend(kq[0])
-            resultreaction.extend(kq[1])
-        try:
-            com = requests.get(com['paging']['next']).json()
-        except:
-            break
-    return (resultcomment,resultreaction)
-def getPost(p):
+    resultreaction = []
     try:
-        return [p['id'], p['message'], p['updated_time']]
+        com = p['comments']
+        while (True):
+            if (len(com['data']) == 0):
+                break
+            for c in com['data']:#['p_id','parent_cid','cid','c_message','c_created_time','uid','uname']
+                resultcomment.append(
+                    [p['id'], 'x', c['id'], c['message'], c['created_time'], c['from']['id'], c['from']['name']])
+                resultreaction.extend(getreaction(p, c))
+
+                kq = getreplyComment(p, c)
+                resultcomment.extend(kq[0])
+                resultreaction.extend(kq[1])
+            try:
+                com = requests.get(com['paging']['next']).json()
+            except:
+                break
+    except:
+        resultcomment = []
+    return (resultcomment,resultreaction)
+
+def getPost(p):#['p_id','p_message','p_created_time','p_updated_time','u_id','u_name','no_share'])
+    msg = ''
+    noshare = 0
+    uid='x'
+    uname='x'
+    try:
+        uid = p['from']['id']
+    except:
+        uid='x'
+
+    try:
+        uname = p['from']['id']
+    except:
+        uname='x'
+
+    try:
+        msg=p['message']
     except:
         try:
-            return [p['id'], p['story'], p['updated_time']]
+            msg=p['story']
         except:
-            return [p['id'], 'x', p['updated_time']]
+            msg='x'
+    try:
+        noshare = p['shares']['count']
+    except:
+        noshare=0
+
+    tempreac = []
+    try:
+        reaction = p['reactions']
+        while (True):
+            if (len(reaction['data']) == 0):
+                break
+            for r in reaction['data']:
+                tempreac.append(
+                    [p['id'], 'x', r['type'], r['id'], r['name']])
+            try:
+                reaction = requests.get(reaction['paging']['next']).json()
+            except:
+                break
+    except:
+        tempreac = []
+    return ([p['id'],p['type'], msg, p['created_time'], p['updated_time'], uid, uname,noshare],tempreac)
+
 
 i = 0
 while(True):
@@ -181,22 +182,21 @@ while(True):
     if (len(posts['data']) == 0):
         break
     print(i)
-    with Pool(4) as pool:
+    with Pool(6) as pool:
         resultspost=pool.map(getPost,posts['data'])
-        resultReaction = pool.map(getreactionsPost, posts['data'])
         kq=pool.map(getcommentPost, posts['data'])
         pool.close()
         pool.join()
-        for p in resultspost:
+
+        for (p,rs) in resultspost:
             writerpost.writerow(p)
-        for p in resultReaction:
-            for r in p:
+            for r in rs:
                 writerreaction.writerow(r)
-        for (c,r) in kq:
-            for ci in c:
-                writercomment.writerow(ci)
-            for ri in r:
-                writerreaction.writerow(ri)
+        for (cs,rs) in kq:
+            for c in cs:
+                writercomment.writerow(c)
+            for r in rs:
+                writerreaction.writerow(r)
 
     try:
         posts = requests.get(posts['paging']['next']).json()
